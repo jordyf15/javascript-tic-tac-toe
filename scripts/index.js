ee7d1e5fde8cfe7767a596be232f94b18f997922
jordyf15 <jordyferdian88@gmail.com>
2021-10-08 14:18:30 +0700

    create GameBoard module

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+const gameBoard = (function() {
+    const _boardGrids = [null,null,null,
+                        null,null,null,
+                        null,null,null];
+
+    function markGrid(grid, mark){
+        const gridIndex = grid-1;
+        _boardGrids[gridIndex] = mark;
+    }
+    function checkLegalGrid(grid) {
+        const gridIndex = grid-1;
+        return _boardGrids[gridIndex] ? false : true;
+    }
+    function checkWin(){
+        for(let i = 0; i<3; i++){
+            if(_boardGrids[(i*3)] == _boardGrids[(i*3)+1] && 
+            _boardGrids[(i*3)+1] == _boardGrids[(i*3)+2]) return true;
+            if(_boardGrids[0+i] == _boardGrids[3+i] &&
+                _boardGrids[3+i] == _boardGrids[6+i]) return true;
+        }
+        if(_boardGrids[0] == _boardGrids[4] && _boardGrids[4] == _boardGrids[8]) return true;
+        if(_boardGrids[2] == _boardGrids[4] && _boardGrids[4] == _boardGrids[6]) return true;
+        return false;
+    }
+
+    return {markGrid, checkLegalGrid, checkWin};
+})();
+
+
